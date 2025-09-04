6b1c0ff3fb4010779b03d226227da30474177ddb
kaste <facugerbino@gmail.com>
2025-09-04 16:23:21 -0300

    build directly to docs

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/octantes.net/', // asegura que los assets apunten al path correcto en GitHub Pages
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/octantes.net/', // correccion del path para ghpages
+  plugins: [ vue(), vueDevTools(), ],
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) },
   },
   build: {
-    outDir: 'dist', // carpeta de build
-    emptyOutDir: true, // limpia dist antes de build
+    outDir: 'docs', // carpeta de build
   },
 })
