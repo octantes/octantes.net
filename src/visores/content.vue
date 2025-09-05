6b210272aaa10fd887b2dd85c99510684575706b
kaste <facugerbino@gmail.com>
2025-09-05 17:19:24 -0300

    unificacion content y markdown

--- src/visores/content.vue
+++ src/visores/content.vue
@@ -1,23 +1,39 @@
 <script setup>
-import Markdown from '../recursos/markdown.vue'
+import { ref, watch } from 'vue'
 import Shader from '../recursos/shader.vue';
 
 const props = defineProps({
     route: String,
 })
+
+const noteContent = ref('')
+
+watch(
+    () => props.route, // react to route change
+    async (route) => {
+        if (!route) return
+        const res = await fetch(import.meta.env.BASE_URL + route)
+        noteContent.value = await res.text()
+    }, { immediate: true }
+)
 </script>
 
 <template>
     <div class="post">
         <Shader v-if="!route" />
-        <Markdown v-else :route="route"/>
+        <div v-else class="text" v-html="noteContent"></div>
     </div>
 </template>
 
 <style>
 .post {
     background-color: #986C98;
-    border: 1px, solid, #AAABAC;
+    border: 1px solid #AAABAC;
     width: 100%;
+}
+.text {
+    color: #1B1C1C;
+    background-color: #986C98;
+    padding: 1rem;
 }
 </style>
